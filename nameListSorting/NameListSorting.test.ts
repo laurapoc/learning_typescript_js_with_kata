c3ec5094af591f3a20c4573a0c86ab41a1be5055
laurapoc <laura.pociuniene@gmail.com>
2021-09-09 16:24:02 +0300

    add test for name list sorting

---
+++ nameListSorting/NameListSorting.test.ts
@@ -0,0 +1,13 @@
+import { meeting } from "./NameListSorting";
+
+describe("meeting", () => {
+  it('returns sorted name list alphabetically: "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"', () => {
+    const sortedNameList = meeting(
+      "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill"
+    );
+
+    expect(sortedNameList).toEqual(
+      "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+    );
+  });
+});
